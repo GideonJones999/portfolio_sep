3c81171a1dcd56e81cc314e34e175c0ca1c8665a
Gideon Jones <gideon.w.jones@gmail.com>
2023-09-28 17:44:36 -0600

    Start with Nav Collapsed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import * as Icon from "react-feather";
 
 export default function Header() {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const [isNavExpanded = false, setIsNavExpanded] = useState(false);
 
   return (
     <div id="header-container">
